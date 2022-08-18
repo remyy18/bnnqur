1c74f97447619bc7ad442aeeb5a8b7fa79a83adc
rizaiman <riza2001@mhs.unsyiah.ac.id>
2022-08-18 09:11:36 +0700

    New repository

---
+++ app/Views/template/sidebar.php
@@ -0,0 +1,153 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">BNN</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        DATA PERUSAHAAN
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+            aria-controls="collapseTwo">
+            <i class="fas fa-building"></i>
+            <span>Profile</span>
+        </a>
+
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Profil Perusahaan</h6>
+                <a class="collapse-item" href="buttons.html">Sejarah Perusahaan</a>
+                <a class="collapse-item" href="cards.html">Nama Pegawai</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        BANK DATA
+    </div>
+
+    <!-- Nav Item -  Menu Umum-->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse1" aria-expanded="true"
+            aria-controls="collapse1">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Sub Umum</span>
+        </a>
+        <div id="collapse1" class="collapse" aria-labelledby="heading1" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Data:</h6>
+                <a class="collapse-item" href="utilities-color.html">Surat Masuk</a>
+                <a class="collapse-item" href="utilities-border.html">Surat Keluar</a>
+                <a class="collapse-item" href="utilities-animation.html">Sprint</a>
+                <a class="collapse-item" href="utilities-other.html">MOU</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Seksi P2M-->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse2" aria-expanded="true"
+            aria-controls="collapse2">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Seksi P2M</span>
+        </a>
+        <div id="collapse2" class="collapse" aria-labelledby="heading2" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Data:</h6>
+                <a class="collapse-item" href="utilities-color.html">Desa Bersinar</a>
+                <a class="collapse-item" href="utilities-border.html">Ketahanan</a>
+                <a class="collapse-item" href="utilities-animation.html">Penyuluhan</a>
+                <a class="collapse-item" href="utilities-other.html">Relawan</a>
+                <a class="collapse-item" href="utilities-other.html">Tes Urin</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Seksi Rehab-->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse3" aria-expanded="true"
+            aria-controls="collapse3">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Seksi Rehab</span>
+        </a>
+        <div id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Data:</h6>
+                <a class="collapse-item" href="utilities-color.html">IBM</a>
+                <a class="collapse-item" href="utilities-border.html">SIL</a>
+                <a class="collapse-item" href="utilities-animation.html">LRKM</a>
+                <a class="collapse-item" href="utilities-other.html">LRIP</a>
+                <a class="collapse-item" href="utilities-other.html">SKHPN</a>
+                <a class="collapse-item" href="utilities-other.html">Rawat Jalan</a>
+                <a class="collapse-item" href="utilities-other.html">Rawat Inap</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Seksi Pemberantasan-->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse4" aria-expanded="true"
+            aria-controls="collapse4">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Seksi Pemberantasan</span>
+        </a>
+        <div id="collapse4" class="collapse" aria-labelledby="heading4" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Data:</h6>
+                <a class="collapse-item" href="utilities-color.html">Tersangka</a>
+                <a class="collapse-item" href="utilities-border.html">Barang Bukti</a>
+            </div>
+        </div>
+    </li>
+
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Profil Pengguna
+    </div>
+
+    <!-- Nav Item - My Profil -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>My Profil</span></a>
+    </li>
+
+    <!-- Nav Item - Edit Profile -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Edit Profil</span></a>
+    </li>
+
+    <!-- Nav Item - Edit Profile -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Log Out</span></a>
+    </li>
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
